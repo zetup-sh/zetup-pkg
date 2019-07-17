6a98b2d371f3b05b3a4ff81064c5b7aaa1e09277
Zane Hitchcox <zwhitchcox@gmail.com>
2019-07-17 12:35:52 -0400

    link dotfiles

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -5,12 +5,13 @@ do
   bn=$(basename $i) ;
   if [[ ! "$bn" = _* ]] ;
     then
-      dotname="$HOME/.${bn%%.*}"
+      dotname="$HOME/.$bn"
       if [ -f "$dotname" ];
 	then
           mv "$dotname" "$dotname.bak"
-          ln -s "$i" "$dotname"
       fi
+      echo $dotname
+      ln -s "$i" "$dotname"
    fi
 done
 
