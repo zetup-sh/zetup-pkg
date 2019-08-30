7aa3c7011e42ff29a47fb6858ae2016c434b23c7
Zane Hitchcox <zwhitchcox@gmail.com>
2019-08-30 10:32:24 -0400

    ssh auto start during tunneling

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -33,23 +33,22 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 SSH_ENV="$HOME/.ssh/environment"
 
-#
-# start ssh agent if it's not already started
-#function start_agent {
-    #/usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    #chmod 600 "${SSH_ENV}"
-    #. "${SSH_ENV}" > /dev/null
-    #/usr/bin/ssh-add;
-#}
+# start ssh agent if it's not already started (necessary when you ssh tunnel)
+function start_agent {
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    chmod 600 "${SSH_ENV}"
+    . "${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add;
+}
 
-## Source SSH settings, if applicable
-#if [ -f "${SSH_ENV}" ]; then
-  #. "${SSH_ENV}" > /dev/null
-  ##ps ${SSH_AGENT_PID} doesn't work under cywgin
-  #ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-      #start_agent;
-  #}
-  #ssh-add "$ZETUP_PRIVATE_KEY_FILE"
-#else
-  #start_agent;
-#fi
+# Source SSH settings, if applicable
+if [ -f "${SSH_ENV}" ]; then
+  . "${SSH_ENV}" > /dev/null
+  #ps ${SSH_AGENT_PID} doesn't work under cywgin
+  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+      start_agent;
+  }
+  ssh-add "$ZETUP_PRIVATE_KEY_FILE"
+else
+  start_agent;
+fi
