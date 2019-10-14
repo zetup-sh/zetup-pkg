4a55b1d986eae6ca2d604e5d79a6d66fb7544390
Zane Hitchcox <zwhitchcox@gmail.com>
2019-10-14 12:44:16 -0400

    docker manjaro socket

--- shell/bashrc.sh
+++ shell/bashrc.sh
@@ -50,3 +50,7 @@ elif [ -z "${SSH_AGENT_PID}" ]; then
   start_agent
   ssh-add "$ZETUP_PRIVATE_KEY_FILE" >/dev/null 2>&1
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
